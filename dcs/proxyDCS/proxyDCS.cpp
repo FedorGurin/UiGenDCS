60dcecbcd20682c30e244f50f6b5276801c13d62
Fedor <gurinfedor@hotmail.com>
2016-06-16 11:54:50 +0300

    + изменение внешнего интерфейса

--- dcs/proxyDCS/proxyDCS.cpp
+++ dcs/proxyDCS/proxyDCS.cpp
@@ -5,11 +5,7 @@ ProxyDCS::ProxyDCS(QObject *parent):QObject(parent)
 {
 
 }
-void ProxyDCS::slotRequest()
-{
-
-}
-bool ProxyDCS::sendRequest(RequestDCS* request)
+void ProxyDCS::slotSendRequest(RequestDCS* req)
 {
 
 }
--- dcs/proxyDCS/proxyDCS.h
+++ dcs/proxyDCS/proxyDCS.h
@@ -14,12 +14,13 @@ class ProxyDCS:public QObject
 {
 public:
     ProxyDCS(QObject* parent = 0);
-    //! отправка запроса для обработки другим модулем
-    bool sendRequest(RequestDCS* request);
-    //void eEvent(RequestEvent*);
 signals:
-    //сигнал посылаемый в случае прихода событий от других приложений
-    void signalReciveRequest();
+    //! получение запроса
+    void signalReciveRequest(RequestDCS* req);
+public slots:
+    //! отправление запроса
+    void slotSendRequest(RequestDCS* req);
+private:
 
 };
 
