b32232f6b57ddddcd37324836d2c2aa039fb2306
Fedor <gurinfedor@hotmail.com>
2016-07-07 17:33:25 +0300

    + добавлена процедура определения порта сокета

--- dcs/proxyDCS/proxyDCS.cpp
+++ dcs/proxyDCS/proxyDCS.cpp
@@ -1,10 +1,49 @@
 #include "proxyDCS.h"
 
+//! начальный базовый порт с которого начинается поиск портов
+#define BASE_PORT 4900
 
 ProxyDCS::ProxyDCS(QObject *parent):QObject(parent)
 {
+    //! порт для выдачи информации другим участникам РС
+    portInfo = 0;
+    //! поиск порта
+    tryFindFreePort();
+}
+bool ProxyDCS::tryFindFreePort()
+{
+    //! настройка сокетов(поиск свободного порта)
+    bool isBind=false;
+    int i=1;
+    do
+    {
+        portInfo=BASE_PORT+i;
+        isBind=udpSockDef.bind(portInfo,QUdpSocket::DefaultForPlatform);
+        i++;
+    }while(isBind == true);
+
+    if(isBind == false)
+    {
+        qDebug()<<tr("ProxyDCS::Can`t find port for binding");
+        return false;
+    }
+
+    qDebug()<<tr("ProxyDCS::Find port for information PortInfo=")<<portInfo;
+
+    //! сохраняем текущую информацию о хосте
+    info=QHostInfo::fromName(QHostInfo::localHostName());
+    //! подключение функции обработки таймера
+    connect(&timerInfo,SIGNAL(timeout()),this,SLOT(slotSendInfo()));
+    //! запуск таймера
+    timerInfo.start(3000);//3сек
+
+    return true;
+}
+void ProxyDCS::slotSendInfo()
+{
 
 }
+
 void ProxyDCS::slotSendRequest(RequestDCS* req)
 {
 
--- dcs/proxyDCS/proxyDCS.h
+++ dcs/proxyDCS/proxyDCS.h
@@ -3,17 +3,35 @@
 
 #include <QObject>
 #include <QByteArray>
+#include <QSharedMemory>
+#include <QHostInfo>
+#include <QUdpSocket>
+#include <QTimer>
+
 // Библиотека обеспечивает взаимодействие модулей между собой
 // у модуля есть два конфигурационных файла io.xml, interfaces.xml
 
+//! статус запроса(в случае невозможности выполнения возвращает ошибку)
+class StatusRequest
+{
+
+};
 //! класс запроса
 class RequestDCS
 {
-    QString id_name;//уникальное имя запроса
-    //! массив данных, которые пользователь сам разбирает в требуемом порядке(или QDataStream?)
-    QByteArray array;
-};
+    //! идентификатор группы (описание в input.xml)
+    QString name_group_id;
 
+    //! циклический запрос или одиночный запрос
+    bool cyclic;
+
+    //! массив данных, которые пользователь сам разбирает в требуемом порядке(или QDataStream?)
+    QByteArray data;
+
+    //! вернуть указатель на параметры
+
+};
+//! Класс подключения к распределенной среде
 class ProxyDCS:public QObject
 {
 public:
@@ -24,6 +42,26 @@ signals:
 public slots:
     //! отправление запроса
     void slotSendRequest(RequestDCS* req);
+
+private slots:
+    //! отправление информации об текущем элементе
+    void slotSendInfo();
+private:
+    //! попытка поиска свободного порта(true - порт найден)
+    bool tryFindFreePort();
+
+    //! список разделяемой памяти входных параметров
+    QVector<QSharedMemory* > sharedMemInput;
+    //! разделяемая память для выходных параметров
+    QSharedMemory sharedMemOutput;
+    //! информация о хосте
+    QHostInfo info;
+    //! порт с выдачей информации об самом себе(выдача каждые 3 сек.)
+    int portInfo;
+    //! порт для самоидентификации
+    QUdpSocket udpSockDef;
+    //! таймер для периодического сообщения о работе модуля(каждые 3 секунды)
+    QTimer timerInfo;
 };
 
 #endif // PROXYDCS_H
