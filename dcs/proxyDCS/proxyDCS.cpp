c18b2856b242d371f5298f3f2c6b64bc2624e4b9
Fedor <gurinfedor@hotmail.com>
2016-07-22 17:54:45 +0300

    + добавлена возможность определения других модулей + реализована передача данных между модулями + в случае потери соединения, информация так же сохраняется
    Signed-off-by: Fedor <gurinfedor@hotmail.com>

--- dcs/proxyDCS/proxyDCS.cpp
+++ dcs/proxyDCS/proxyDCS.cpp
@@ -8,13 +8,16 @@
 //! базовый порт использующийся при старте приложения
 #define BASE_PORT_STARTING 4901
 
-
+#define TIME_INFO 3000 //3sec
+#define TIME_LOST_CONNECT 6500
 ProxyDCS::ProxyDCS(QObject *parent):QObject(parent)
 {
     //! настройка генератора
     qsrand(QTime::currentTime().msec());
     //! подключение к разделяемому порту
     bool bindShared = false;
+    //! признак ожидания и только прослушивания информации от других модулей
+    listeningInfo = true;
     //! обнуление обнаруженных модулей
     infoModules.clear();
     //! здесь нужно сформировать универсальный идентификатор
@@ -25,23 +28,55 @@ ProxyDCS::ProxyDCS(QObject *parent):QObject(parent)
     //! общий порт для получения данных о загруженных модулях
     portShare = BASE_PORT_STARTING;
     bindShared = udpSockDef.bind(QHostAddress::LocalHost,portShare,QUdpSocket::ReuseAddressHint);
+
     if(bindShared == false)
     {
         qDebug()<<tr("ProxyDCS::Can`t bind to BASE_PORT_SHARED=")<<BASE_PORT_STARTING;
-        qDebug()<<tr("Closing application");
+        qDebug()<<tr("ProxyDCS::Error = ")<<udpSockDef.errorString();
+        qDebug()<<tr("ProxyDCS::Paused");
         return;
     }
 
     //! поиск свободного порта
-    tryFindFreePort();
+    bool result = tryFindFreePort();
 
+    if(result == true)
+        connect(&udpSockData,SIGNAL(readyRead()),this,SLOT(slotReciveFromDataPort()));
     //! получение идентификационных данных от других модулей
-    connect(&udpSockDef,SIGNAL(readyRead()),this,SLOT(slotReciveInfo()));
+    connect(&udpSockDef,SIGNAL(readyRead()),this,SLOT(slotReciveFromSharePort()));
+
+    //! Только для проверки
+    connect(&udpSockDef,SIGNAL(connected()),this,SLOT(checkSLOT()));
 
 }
-void ProxyDCS::slotReciveInfo()
+void ProxyDCS::checkSLOT()
+{
+    qDebug()<<"SLOT";
+
+}
+
+void ProxyDCS::slotReciveFromDataPort()
 {
     QByteArray datagram;
+    do
+    {
+        datagram.resize(udpSockData.pendingDatagramSize());
+        quint64 size = udpSockData.readDatagram(datagram.data(),
+                                               datagram.size());
+        if(size == -1)
+            qDebug()<<tr("ProxyDCS::Can`t readDatagram from socket");
+
+    }while  (udpSockData.hasPendingDatagrams());
+
+    processPacket(datagram);
+}
+
+void ProxyDCS::slotReciveFromSharePort()
+{
+    QByteArray datagram;
+
+    //! нужно перестать слушать, перейти в состояние передачи информации всем узлам
+    listeningInfo = false;
     do
     {
         datagram.resize(udpSockDef.pendingDatagramSize());
@@ -51,40 +86,65 @@ void ProxyDCS::slotReciveInfo()
             qDebug()<<tr("ProxyDCS::Can`t readDatagram from socket");
 
     }while  (udpSockDef.hasPendingDatagrams());
+    processPacket(datagram);
+}
+void ProxyDCS::parseInfo(TInfoPacket& recivePacket)
+{
+    for(int i=0;i<recivePacket.sizeAddr;i++)
+    {
+        bool findModule=false;
+        if(recivePacket.addr[i].uid_module == info.uid_module)
+            continue;
+        //! поиск уже сохраненной информации в списке
+        for(int j=0;j<infoModules.size();j++)
+        {
+            if(recivePacket.addr[i].uid_module == infoModules[j]->uid_module)
+            {
+                infoModules[j]->lostConnect = false;
+                infoModules[j]->secLastConnect = 0.0;
+                findModule = true;
+            }
+        }
+        if(findModule == false)
+        {
+            //! сохраняем модуль в спсике
+            DefineAddr *newAddr = new DefineAddr;
+            newAddr->ip = QString(recivePacket.addr[i].ip);
 
+            newAddr->portModule = recivePacket.addr[i].portModule;
+            newAddr->uid_module = recivePacket.addr[i].uid_module;
+            newAddr->secLastConnect = 0;
+            newAddr->lostConnect = false;
+
+            infoModules.append(newAddr);
+            qDebug()<<tr("ProxyDCS::add new Module to list, id_module=")<<newAddr->uid_module;
+        }
+    }
+    if(listeningInfo == false)
+        slotSendInfoAll();
+}
+
+void ProxyDCS::processPacket(QByteArray& datagram)
+{
     QDataStream outHead(&datagram,QIODevice::ReadOnly);
     outHead.setVersion(QDataStream::Qt_4_2);
     outHead.readRawData((char*)&headPacket,sizeof(THeadPacket));
 
     //! обнаружено эхо
-    if(headPacket.uid == info.uid_module)
+    if(headPacket.uid_module == info.uid_module)
     {
         qDebug()<<tr("ProxyDCS::echo detected");
         return;
     }
 
     QDataStream out(&datagram,QIODevice::ReadOnly);
-    out.setVersion(QDataStream::Qt_4_2);
+    //out.setVersion(QDataStream::Qt_4_2);
 
-    out.readRawData((char*)&infoRecive,headPacket.size);
-    processInfoRecive(infoRecive);
-}
-void ProxyDCS::processInfoRecive(TInfoPacket& recivePacket)
-{
-    //! поиск уже сохраненной информации в списке
-    for(int i=0;i<infoModules.size();i++)
+    if(headPacket.type == 0)
     {
-        if(recivePacket.head.uid == infoModules[i]->uid_module)
-            return;
+          out.readRawData((char*)&infoRecive,headPacket.size);
+          parseInfo(infoRecive);
     }
-
-    //! сохраняем модуль в спсике
-    DefineAddr *newAddr = new DefineAddr;
-    newAddr->ip = QString(recivePacket.ip);
-    newAddr->portModule = recivePacket.portModule;
-    newAddr->uid_module = recivePacket.head.uid;
-
-    qDebug()<<tr("ProxyDCS::add new Module to list");
 }
 
 //! отправляю свои идентификационные данные
@@ -118,24 +178,83 @@ bool ProxyDCS::tryFindFreePort()
     QList<QHostAddress> list = info.info.addresses();
     info.ip = list[0].toString();
     //! подключение функции обработки таймера
-    connect(&timerInfo,SIGNAL(timeout()),this,SLOT(slotSendInfo()));
+    connect(&timerInfo,SIGNAL(timeout()),this,SLOT(slotSendInfoOwn()));
     //! запуск таймера
-    timerInfo.start(3000);//3сек
+    timerInfo.start(TIME_INFO);//3сек
 
     return true;
 }
-//! отправление информации о текущем модуле
-void ProxyDCS::slotSendInfo()
+//! проверка есть ли потеря соединения с узлами
+void ProxyDCS::checkLostConnect()
 {
-    infoPacket.head.uid = info.uid_module;
+    //! поиск уже сохраненной информации в списке
+    for(int i=0;i<infoModules.size();i++)
+    {
+        infoModules[i]->secLastConnect += TIME_INFO;
+        if(infoModules[i]->lostConnect == true)
+            continue;
+        if(infoModules[i]->secLastConnect>TIME_LOST_CONNECT)
+        {
+            infoModules[i]->lostConnect = true;
+            qDebug()<<tr("ProxyDCS::lost connection with id_module = ")<<infoModules[i]->uid_module;
+        }
+    }
+}
+//! отправление информации о текущем модуле
+void ProxyDCS::slotSendInfoAll()
+{
+    TInfoPacket infoForAll;
+    int size = infoModules.size() + 1;
+    infoForAll.head.size = sizeof(THeadPacket) +  sizeof(int) + size*sizeof(TAddr);
+    infoForAll.head.type = 0;
+    infoForAll.head.uid_module = info.uid_module;
+    infoForAll.sizeAddr = size;
+
+    //! добавляем текущий модуль
+    infoForAll.addr[0].uid_module = info.uid_module;
+    infoForAll.addr[0].portModule = info.portModule;
+    strcpy(infoPacket.addr[0].ip,(const char*)info.ip.toLocal8Bit().constData());
+
+    for(int i=0;i<infoModules.size();i++)
+    {
+        infoForAll.addr[i+1].uid_module = infoModules[i]->uid_module;
+        infoForAll.addr[i+1].portModule = infoModules[i]->portModule;
+        strcpy(infoForAll.addr[i+1].ip,(const char*)infoModules[i]->ip.toLocal8Bit().constData());
+    }
+
+    for(int i=0;i<infoModules.size();i++)
+    {
+        if(infoModules[i]->uid_module != info.uid_module && infoModules[i]->lostConnect == false)
+        {
+            //! отправляем данные
+            quint64 size=udpSockData.writeDatagram((char*)&infoForAll, infoForAll.head.size,
+                                                   QHostAddress::LocalHost,
+                                                   infoModules[i]->portModule);
+
+            if(size == -1)
+                qDebug()<<tr("ProxyDCS::Can`t send datainfo");
+        }
+    }
+}
+
+//! отправление информации о текущем модуле
+void ProxyDCS::slotSendInfoOwn()
+{
+    checkLostConnect();
+
+    infoPacket.sizeAddr = 1;
+
+    infoPacket.head.uid_module = info.uid_module;
     infoPacket.head.type = 0;
-    infoPacket.head.size = sizeof(TInfoPacket);
+    infoPacket.head.size =  sizeof(THeadPacket) +  sizeof(int) + infoPacket.sizeAddr*sizeof(TAddr);
 
-    infoPacket.portModule = info.portModule;
-    strcpy(infoPacket.ip,(const char*)info.ip.toLocal8Bit().constData());
+    infoPacket.addr[0].uid_module = info.uid_module;
+    infoPacket.addr[0].portModule = info.portModule;
+    strcpy(infoPacket.addr[0].ip,(const char*)info.ip.toLocal8Bit().constData());
 
 
-    qDebug()<<"3 sec left";
+    int status = udpSockDef.state();
+    //qDebug()<<tr("ProxyDCS::sendInfo(3 sec left)");
     quint64 size=udpSockDef.writeDatagram((char*)&infoPacket, infoPacket.head.size,
                                            QHostAddress::LocalHost,
                                            portShare);
--- dcs/proxyDCS/proxyDCS.h
+++ dcs/proxyDCS/proxyDCS.h
@@ -11,6 +11,8 @@
 // Библиотека обеспечивает взаимодействие модулей между собой
 // у модуля есть два конфигурационных файла io.xml, interfaces.xml
 
+#define MAX_ADDR_IN_PACKET 16
+#define LENGTH_IP_STRING 64
 //! класс с информацией об модуле
 struct DefineAddr
 {
@@ -22,27 +24,41 @@ struct DefineAddr
     QString ip;
     //! информация о хосте
     QHostInfo info;
+    //! кол-во секунд после предыдущего обнавления
+    unsigned secLastConnect;
+    //! признак потери соединения с узлом
+    bool lostConnect;
 };
 //! заголовок запроса
 typedef struct THeadPacket_
 {
-    //! глобальный уникальный идентификатор модуля
-    quint64 uid;
+    //! глобальный уникальный идентификатор модуля отправителя
+    quint64 uid_module;
     //! Тип сообщения
-    char type;
+    char type; // 0 - пакет с информацией об узле
     //! размер пакета
     unsigned long size;
 }THeadPacket;
+
+typedef struct TAddr_
+{
+    //! идентификатор модуля
+    quint64 uid_module;
+    //! порт для работы с модулем
+    int portModule;
+    //! ip - адрес приложения
+    char ip[LENGTH_IP_STRING];
+}TAddr;
 
 //! Запрос с информацией об модуле
 typedef struct TInfoPacket_
 {
     //! заголовок пакета
     THeadPacket head;
-    //! порт для работы с модулем
-    int portModule;
-    //! ip - адрес приложения
-    char ip[64];
+    //! кол-во адресов в спсике
+    int sizeAddr;
+    //! список адресов
+    TAddr addr[MAX_ADDR_IN_PACKET];
 }TInfoPacket;
 
 //! статус запроса(в случае невозможности выполнения возвращает ошибку)
@@ -79,15 +95,26 @@ public slots:
     void slotSendRequest(RequestDCS* req);
 private slots:
     //! отправление информации об текущем элементе
-    void slotSendInfo();
-    void slotReciveInfo();
+    void slotSendInfoOwn();
+    //! отправить информации всем участникам среды
+    void slotSendInfoAll();
+    //! получение данных от других источников
+    void slotReciveFromSharePort();
+    void slotReciveFromDataPort();
     //! попытка сообщить о себе другим участикам
     //void slotTryToSpeak();
+
+    void checkSLOT();
 private:
-    //! попытка поиска свободного порта(true - порт найден)
+    //! попытка поиска свободных портов для отправки и получения(true - порт найден)
     bool tryFindFreePort();
+    //! проверка есть ли потеря соединения с узлами
+    void checkLostConnect();
+
     //! разбор полученного пакета
-    void processInfoRecive(TInfoPacket& recivePacket);
+    void processPacket(QByteArray& datagram);
+    //! разбор пакета с информацией
+    void parseInfo(TInfoPacket& recivePacket);
     //! список обнаруженных модулей
     QVector<DefineAddr * > infoModules;
     //! глобальный уникальный идентификатор модуля
@@ -100,6 +127,8 @@ private:
     DefineAddr info;
     //! общий порт для всех приложений передаем информацию о приложении(выдача каждые 3 сек.)
     int portShare;
+    //! признак ожидания и только прослушивания информации от других модулей
+    bool listeningInfo;
     //! порт для самоидентификации
     QUdpSocket udpSockDef;
     QUdpSocket udpSockData;
